288585da8bdeb32686978f528cc8ed4b906fae24
Brane Petrovic <branep@gmail.com>
2024-07-01 15:04:46 -0400

    working conf and tag

--- ovos_lang_detector_fasttext_local_server_plugin/__init__.py
+++ ovos_lang_detector_fasttext_local_server_plugin/__init__.py
@@ -1,4 +1,6 @@
 from ovos_plugin_manager.templates.language import LanguageDetector
+from ovos_config.config import Configuration
+from langcodes import standardize_tag
 import requests
 
 
@@ -10,15 +12,16 @@ class FastTextLangServerDetectPlugin(LanguageDetector):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.url = Configuration().get(
+            "FastTextLangServerDetectPlugin", {}).get(
+                "url",
+                "http://10.0.1.44:8000"
+                )
 
     def detect(self, text):
-        url = self.settings.get(
-            "language_detect_fqdn",
-            "http://10.0.1.44:8000"
-            )
         try:
-            resp = requests.get(f"{url}/language_detect?text={text}")
-            return resp.json()[0][0]
+            resp = requests.get(f"{self.url}/language_detect?text={text}")
+            return standardize_tag(resp.json()[0][0])
         except requests.exceptions.RequestException as e:
             self.log.error("TTS Lang detect error: %s", e)
             return ""
